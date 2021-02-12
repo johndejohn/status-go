c867b4b85d98146b1afc6132531a5ee6270dbe05
Samuel Hawksby-Robinson <samuel@samyoul.com>
2021-02-12 15:55:34 +0000

    Updated tests to include tests on IdentityImage.Encrypted state

--- protocol/messenger_test.go
+++ protocol/messenger_test.go
@@ -2613,6 +2613,8 @@ func (s *MessengerSuite) TestEncryptDecryptIdentityImagesWithContactPubKeys() {
 	}
 	s.Require().NotEqual([]byte(smPayload), ci.Images["small"].Payload)
 	s.Require().NotEqual([]byte(lgPayload), ci.Images["large"].Payload)
+	s.Require().True(ci.Images["small"].Encrypted)
+	s.Require().True(ci.Images["large"].Encrypted)
 
 	// Switch messenger identities
 	sender := s.m.identity
@@ -2624,6 +2626,8 @@ func (s *MessengerSuite) TestEncryptDecryptIdentityImagesWithContactPubKeys() {
 
 	s.Require().Equal(smPayload, string(ci.Images["small"].Payload))
 	s.Require().Equal(lgPayload, string(ci.Images["large"].Payload))
+	s.Require().False(ci.Images["small"].Encrypted)
+	s.Require().False(ci.Images["large"].Encrypted)
 
 	// RESET Messenger identity, Contacts and IdentityImage.EncryptionKeys
 	s.m.identity = sender
@@ -2640,6 +2644,8 @@ func (s *MessengerSuite) TestEncryptDecryptIdentityImagesWithContactPubKeys() {
 	}
 	s.Require().NotEqual([]byte(smPayload), ci.Images["small"].Payload)
 	s.Require().NotEqual([]byte(lgPayload), ci.Images["large"].Payload)
+	s.Require().True(ci.Images["small"].Encrypted)
+	s.Require().True(ci.Images["large"].Encrypted)
 
 	// Switch messenger identities
 	s.m.identity = contactKeys[2]
@@ -2650,6 +2656,8 @@ func (s *MessengerSuite) TestEncryptDecryptIdentityImagesWithContactPubKeys() {
 
 	s.Require().NotEqual([]byte(smPayload), ci.Images["small"].Payload)
 	s.Require().NotEqual([]byte(lgPayload), ci.Images["large"].Payload)
+	s.Require().True(ci.Images["small"].Encrypted)
+	s.Require().True(ci.Images["large"].Encrypted)
 
 	// RESET Messenger identity
 	s.m.identity = sender
